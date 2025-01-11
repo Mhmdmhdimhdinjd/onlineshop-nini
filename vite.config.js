742628a2c288fef6bb6ca455870888e5a68cdf19
mhmd mhdi <mhdimhdin5@gmail.com>
2025-01-11 13:48:31 +0330

    add deploy file & complete vite.config

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,8 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base:'/onlineshop-nini/',
+  build:{
+    outDir:'dist'
+  }
 })
